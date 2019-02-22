c5f7664705d7d1e4c32041d1a44e4ab7cadc758f
hubzhangxj <zhangxinjia@huawei.com>
2019-02-22 14:59:41 +0800

    add x86 support and recover the bug of firewall detection

--- envcheck/envcheck_test.sh
+++ envcheck/envcheck_test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 ###################################################################################
+arch=`uname -m`
+###################################################################################
 # Usage
 ###################################################################################
 Usage()
@@ -10,7 +12,7 @@ Instruction:
     1. Check firewalld stauts, and stop it for network testing;
     2. Install the dependency packages required for testing;
        .e.g. numactl
-    3. Curently Only the following OS is suppoeted, CentOS Kylin and RedHat;
+    3. Curently Only the following OS is suppoeted, CentOS NeoKylin and RedHat;
 EOF
 }
 
@@ -30,7 +32,7 @@ fire_stat=`systemctl status firewalld | grep Active | awk '{print $2}'`
 if which numactl ; then
     echo -e "\n   numactl has install    \n"
 else
-   rpm -ivh numactl-2.0.9-7.el7.aarch64.rpm  
+   rpm -ivh numactl-2.0.9-7.el7.${arch}.rpm  
 fi
 
 if [ x"$fire_stat" = x"inactive" ] ; then
@@ -40,8 +42,9 @@ else
     echo -e "\n   firewalld active , execute cmd to stop it  \n"
     systemctl stop firewalld
     sleep 3 
+    fire_stat=`systemctl status firewalld | grep Active | awk '{print $2}'`
     if [ x"$fire_stat" = x"inactive" ] ; then
-        echo -e " firewalld inactive \n"
+        echo -e "   firewalld has been inactive \n"
         exit 1
     else                                         
         echo -e " firewalld cannot stop , maybe execute cmd [systemctl stop firewalld] to stop it manully  \n"
